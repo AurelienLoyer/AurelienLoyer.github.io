d372a210f9c1aafc48642467ba83eb4c2f313395
Aurélien Loyer <aur.loy@gmail.com>
2018-05-06 16:29:34 +0200

    chore: clean code

--- src/App.vue
+++ src/App.vue
@@ -3,50 +3,44 @@
     <v-header></v-header>
     <v-home></v-home>
     <v-about></v-about>
+    <v-dates></v-dates>
     <v-footer></v-footer>
   </div>
 </template>
 
 <script>
 
-//import 'font-awesome/css/font-awesome.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
+  import Header from './components/Header.vue'
+  import Home from './components/Home.vue'
+  import About from './components/About.vue'
+  import Dates from './components/Dates.vue'
+  import Footer from './components/Footer.vue'
 
-import Header from './components/Header.vue'
-import Home from './components/Home.vue'
-import About from './components/About.vue'
-import Works from './components/Works.vue'
-import Maps from './components/Maps.vue'
-import Contact from './components/Contact.vue'
-import Footer from './components/Footer.vue'
-
-export default {
-  name: 'app',
-  components: {
-    'v-header': Header,
-    'v-home': Home,
-    'v-about': About,
-    'v-works': Works,
-    'v-maps': Maps,
-    'v-contact': Contact,
-    'v-footer': Footer
+  export default {
+    name: 'app',
+    components: {
+      'v-header': Header,
+      'v-home': Home,
+      'v-about': About,
+      'v-dates': Dates,
+      'v-footer': Footer
+    }
   }
-}
 </script>
 
 <style lang="scss">
-
-html{
+  html {
     background: black;
-}
+  }
 
-html, body{
+  html,
+  body {
     margin: 0;
     padding: 0;
     height: 100%;
-}
+  }
 
-body {
+  body {
     font-size: 100%;
     margin: 0;
     padding: 0;
@@ -58,50 +52,58 @@ body {
     -moz-transition: all 0.5s;
     -o-transition: all 0.5s;
     transition: all 0.5s;
-}
+  }
 
-body.menu-open{
+  body.menu-open {
     margin-left: -50%;
     overflow: hidden;
     width: 100%;
-}
+  }
 
-a,a:hover{
+  a,
+  a:hover {
     text-decoration: none;
-}
+  }
 
-body h1,body h2,body h3{
+  body h1,
+  body h2,
+  body h3 {
     padding: 0;
     margin: 0;
-    display:inline-block;
+    display: inline-block;
     color: white;
-}
+  }
 
-p{
+  p {
     margin: 0;
     padding: 0;
-}
+  }
 
-section{
+  section {
     -webkit-transition: all 0.5s;
     -moz-transition: all 0.5s;
     -o-transition: all 0.5s;
     transition: all 0.5s;
-}
+  }
 
-.trans05{
+  .trans05 {
     -webkit-transition: all 0.5s;
     -moz-transition: all 0.5s;
     -o-transition: all 0.5s;
     transition: all 0.5s;
-}
+  }
 
-.animate {
-    opacity:0;
+  .trans02 {
+    -webkit-transition: all 0.2s;
+    -moz-transition: all 0.2s;
+    -o-transition: all 0.2s;
+    transition: all 0.2s;
+  }
+
+  .animate {
+    opacity: 0;
     -webkit-transform: translateX(100px) translateY(50px);
     -moz-transform: translateX(100px) translateY(50px);
     transform: translateX(100px) translateY(50px);
-}
-
-
+  }
 </style>
