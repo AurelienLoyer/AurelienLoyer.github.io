faba9492e25fd20b8bdd8d1bc23dc602691e0f5a
Aurélien Loyer <aur.loy@gmail.com>
2021-03-22 22:30:00 +0100

    feat: rework design

--- README.md
+++ README.md
@@ -1,27 +1,2 @@
-# Personal website build using VueJS
+# Personal website build using just HTML and CSS
 
-## Description
-
-### VueJS version
-https://aurelien-loyer.fr/
-https://t3kstiil3.github.io/
-
-### old front Wordpress version
-https://wp.aurelien-loyer.fr/
-
-### Wordpress API v2
-
-In this version i'm using the wordpress api v2 to provide my vuejs app :)
-
-## Build Setup
-
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-```
--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,42 @@
-var dataCacheName = 'AurelienLoyerData';
-var cacheName = 'AurelienLoyerCache';
-var filesToCache = [
-  'dist/build.js',
-  'dist/build.js.map',
-  'src/assets/dolorean_w.svg',
-  'src/assets/earth.svg',
-  'src/assets/moi_picto.png',
-  'src/assets/github.png',
-  'src/assets/twitter.png',
-  'src/assets/linkedin.png',
-  'src/assets/icons-192.png',
-  'index.html',
-  'favicon.ico',
-];
+var dataCacheName = "AurelienLoyerData";
+var cacheName = "AurelienLoyerCache";
+var filesToCache = ["src/assets/icons-192.png", "index.html", "favicon.ico"];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+self.addEventListener("install", function (e) {
+  console.log("[ServiceWorker] Install");
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
+    caches.open(cacheName).then(function (cache) {
+      console.log("[ServiceWorker] Caching app shell");
       return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+self.addEventListener("activate", function (e) {
+  console.log("[ServiceWorker] Activate");
   e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          if (key !== cacheName && key !== dataCacheName) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
-  
+
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://wp.aurelien-loyer.fr';
+self.addEventListener("fetch", function (e) {
+  console.log("[Service Worker] Fetch", e.request.url);
+  var dataUrl = "https://wp.aurelien-loyer.fr";
   if (e.request.url.indexOf(dataUrl) > -1) {
     e.respondWith(
-      caches.open(dataCacheName).then(function(cache) {
-        return fetch(e.request).then(function(response){
+      caches.open(dataCacheName).then(function (cache) {
+        return fetch(e.request).then(function (response) {
           cache.put(e.request.url, response.clone());
           return response;
         });
@@ -54,7 +44,7 @@ self.addEventListener('fetch', function(e) {
     );
   } else {
     e.respondWith(
-      caches.match(e.request).then(function(response) {
+      caches.match(e.request).then(function (response) {
         return response || fetch(e.request);
       })
     );
